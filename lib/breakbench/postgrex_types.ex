5c882bc0b245eb9abdad060298fcc1ff9080007f
virayatta <te.virayatta@gmail.com>
2018-09-11 22:58:37 +1000

    Re-format

--- lib/breakbench/postgrex_types.ex
+++ lib/breakbench/postgrex_types.ex
@@ -1,3 +1,8 @@
-Postgrex.Types.define(Breakbench.PostgresTypes,
-  [Geo.PostGIS.Extension] ++ Ecto.Adapters.Postgres.extensions(),
-  json: Poison)
+postgrex_extensions = Ecto.Adapters.Postgres.extensions()
+external_extensions = [
+  Geo.PostGIS.Extension
+]
+
+Postgrex.Types.define Breakbench.PostgresTypes,
+  external_extensions ++ postgrex_extensions,
+  json: Poison
