881299310ebcc87356e6a02aa1b82ea23682fe2d
virayatta <te.virayatta@gmail.com>
2018-09-14 09:00:06 +1000

    Remove Bootstrap

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -18,7 +18,6 @@ module.exports = (env, options) => ({
   },
   entry: [
     'babel-polyfill',
-    'bootstrap-loader',
     './js/app.js'
   ],
   output: {
@@ -70,18 +69,7 @@ module.exports = (env, options) => ({
       'window.jQuery': 'jquery',
       Tether: 'tether',
       'window.Tether': 'tether',
-      Popper: ['popper.js', 'default'],
-      Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
-      Button: 'exports-loader?Button!bootstrap/js/dist/button',
-      Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
-      Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
-      Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
-      Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
-      Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
-      Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
-      Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
-      Tooltip: 'exports-loader?Tooltip!bootstrap/js/dist/tooltip',
-      Util: 'exports-loader?Util!bootstrap/js/dist/util',
+      Popper: ['popper.js', 'default']
     }),
   ]
 });
